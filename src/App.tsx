053a1adcd6efa5efec21f301da7ddc56f58f2e37
RodrigoCortezDev <rodrigocortezdev@gmail.com>
2020-05-24 16:26:46 -0300

    Estilos Globais

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,16 @@
 import React from 'react';
 import { BrowserRouter } from 'react-router-dom';
 import Routes from './routes';
+import GlobalStyle from './styles/global';
 
 const App: React.FC = () => (
-    <BrowserRouter>
-        <Routes />
-    </BrowserRouter>
+    <>
+        {/* Caso não tenha incorpora a URL base da aplicação */}
+        <BrowserRouter>
+            <Routes />
+        </BrowserRouter>
+        <GlobalStyle />
+    </>
 );
 
 export default App;
