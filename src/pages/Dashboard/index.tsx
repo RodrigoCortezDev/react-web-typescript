bd0fe3cfd23218b49a009e392e04466acb0ba902
RodrigoCortezDev <rodrigocortezdev@gmail.com>
2020-05-24 23:07:43 -0300

    Finalizando DashBoard

--- src/pages/Dashboard/index.tsx
+++ src/pages/Dashboard/index.tsx
@@ -1,58 +1,89 @@
-import React from 'react';
+import React, { useState, FormEvent, useEffect } from 'react';
 import { FiChevronRight } from 'react-icons/fi';
-import { Title, Form, Repositories } from './styles';
+import api from '../../services/api';
+
+import { Title, Form, Repositories, Error } from './styles';
 import logoImg from '../../assets/logo.svg';
 
+interface Repository {
+    full_name: string;
+    description: string;
+    owner: {
+        login: string;
+        avatar_url: string;
+    };
+}
+
 const Dashboard: React.FC = () => {
+    const [newRep, setNewRepo] = useState('');
+    const [inputError, setInputError] = useState('');
+
+    // no momento set do valor padrão ja tenta buscar do localstorage
+    const [repositories, setRepositories] = useState<Repository[]>(() => {
+        // tenta buscar do localstorage valores anteriores
+        const storageRepositories = localStorage.getItem('@ReactWebTypescript:repositories');
+        // Se já existe valor faz um parte e devolve, senão volta o valor defavult []
+        if (storageRepositories) return JSON.parse(storageRepositories);
+        return [];
+    });
+
+    // Hooks que é chamado toda vez que o array repositories é disparado
+    useEffect(() => {
+        localStorage.setItem('@ReactWebTypescript:repositories', JSON.stringify(repositories));
+    }, [repositories]);
+
+    // Função que pega o que foi digitado e faz uma consulta na API do GIT e adiciona o array
+    // E consequentemente ja chama o useEffect que adiciona no localstorage.
+    async function handleAddRepository(ev: FormEvent<HTMLFormElement>): Promise<void> {
+        ev.preventDefault();
+
+        if (!newRep) {
+            setInputError('Digite o nome do autor/repositorio');
+            return;
+        }
+
+        try {
+            const response = await api.get<Repository>(`repos/${newRep}`);
+            const repository = response.data;
+
+            setRepositories([...repositories, repository]);
+            setNewRepo('');
+            setInputError('');
+        } catch (error) {
+            setInputError('Erro na busca pelo repositorio!');
+        }
+    }
+
+    // Processamento da tela
     return (
         <>
             <img src={logoImg} alt="Github Exporer" />
             <Title>Explorar repositórios do Git.</Title>
 
-            <Form>
-                <input placeholder="Digite o nome do repositório" />
+            <Form hasError={!!inputError} onSubmit={handleAddRepository}>
+                <input
+                    value={newRep}
+                    onChange={(e) => setNewRepo(e.target.value)}
+                    placeholder="Digite o nome do repositório"
+                />
                 <button type="submit">Pesquisar</button>
             </Form>
 
+            {/* Aqui se o inputErro tem valor ele mostra o elemento */}
+            {inputError && <Error>{inputError}</Error>}
+
             <Repositories>
-                <a href="teste">
-                    <img
-                        src="https://avatars0.githubusercontent.com/u/59732403?s=460&u=abdc810569d69ee86cce89f18b3bd2d262d5d21d&v=4"
-                        alt="Rodrigo"
-                    />
-                    <div>
-                        <strong>Rockeseat</strong>
-                        <p>asdadasd asasdasdad asdas asd asda asdasdasd adada aas a a as a ad asasdasdasdasd</p>
-                    </div>
+                {repositories.map((repository) => (
+                    <a key={repository.full_name} href="teste">
+                        <img src={repository.owner.avatar_url} alt={repository.owner.login} />
+                        <div>
+                            <strong>{repository.full_name}</strong>
+                            <p>{repository.description}</p>
+                        </div>
 
-                    <FiChevronRight size={20} />
-                </a>
-
-                <a href="teste">
-                    <img
-                        src="https://avatars0.githubusercontent.com/u/59732403?s=460&u=abdc810569d69ee86cce89f18b3bd2d262d5d21d&v=4"
-                        alt="Rodrigo"
-                    />
-                    <div>
-                        <strong>Rockeseat</strong>
-                        <p>asdadasd asasdasdad asdas asd asda asdasdasd adada aas a a as a ad asasdasdasdasd</p>
-                    </div>
-
-                    <FiChevronRight size={20} />
-                </a>
-
-                <a href="teste">
-                    <img
-                        src="https://avatars0.githubusercontent.com/u/59732403?s=460&u=abdc810569d69ee86cce89f18b3bd2d262d5d21d&v=4"
-                        alt="Rodrigo"
-                    />
-                    <div>
-                        <strong>Rockeseat</strong>
-                        <p>asdadasd asasdasdad asdas asd asda asdasdasd adada aas a a as a ad asasdasdasdasd</p>
-                    </div>
-
-                    <FiChevronRight size={20} />
-                </a>
+                        <FiChevronRight size={20} />
+                    </a>
+                ))}
             </Repositories>
         </>
     );
--- src/pages/Dashboard/styles.ts
+++ src/pages/Dashboard/styles.ts
@@ -1,6 +1,14 @@
-import styled from 'styled-components';
+import styled, { css } from 'styled-components';
 import { shade } from 'polished';
 
+// INTERFACES
+// Interface para ser usada pois todo parametro extra que precisa criar
+// pra trazer algum dado da pagina aqu pro estilo precisa ter interface
+interface FormProps {
+    hasError: boolean;
+}
+
+// ESTILOS
 export const Title = styled.h1`
     font-size: 48px;
     color: #3a3a3a;
@@ -9,7 +17,7 @@ export const Title = styled.h1`
     margin-top: 80px;
 `;
 
-export const Form = styled.form`
+export const Form = styled.form<FormProps>`
     margin-top: 40px;
     max-width: 700px;
     display: flex;
@@ -21,6 +29,14 @@ export const Form = styled.form`
         border: 0;
         border-radius: 5px 0 0 5px;
         color: #3a3a3a;
+        border: 2px solid #fff;
+
+        /* Caso o FormProps seja verdadeiro, estiliza a borda de forma diferente */
+        ${(props) =>
+            props.hasError &&
+            css`
+                border-color: #c53030;
+            `}
 
         &::placeholder {
             color: '#a8a8b3';
@@ -41,6 +57,12 @@ export const Form = styled.form`
             background: ${shade(0.2, '#04d361')};
         }
     }
+`;
+
+export const Error = styled.span`
+    display: block;
+    color: #c53030;
+    margin-top: 8px;
 `;
 
 export const Repositories = styled.div`
@@ -74,7 +96,8 @@ export const Repositories = styled.div`
         }
 
         div {
-            margin-left: 16px;
+            margin: 0 16px;
+            flex: 1;
 
             strong {
                 font-size: 20px;
